b6881a743d2361a6381370638898f91293a42c98
Turkan Isayeva <turkantech@gmail.com>
2026-03-22 06:56:04 +0400

    created json file from embeddings of words

--- src/emb/index.ts
+++ src/emb/index.ts
@@ -1,7 +1,15 @@
+import { readFileSync, writeFileSync } from "fs";
 import { OpenAI } from "openai";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const client = new OpenAI();
-
+type EmbeddingsData = {
+  input: string;
+  embedding: number[];
+};
 async function createEmbeddings(input: string | string[]) {
   const response = await client.embeddings.create({
     input: input,
@@ -12,4 +20,34 @@ async function createEmbeddings(input: string | string[]) {
   return response;
 }
 
-createEmbeddings(["cat is an animal " , "dog is also an animal"]);
+createEmbeddings(["cat is an animal ", "dog is also an animal"]);
+
+function loadJson<T>(filename: string): T {
+  const path = join(__dirname, filename);
+  const rawData = readFileSync(path);
+  return JSON.parse(rawData.toString());
+}
+
+function saveDataToJsonFile(data: any, filename: string) {
+  const dataString = JSON.stringify(data, null, 4);
+  const dataObject = Buffer.from(dataString);
+  const path = join(__dirname, filename);
+
+  writeFileSync(path, dataObject);
+  console.log(`Save data to ${filename}`);
+}
+
+async function createJsonFile() {
+  const data = loadJson<string[]>("index.json");
+  const embeddings = await createEmbeddings(data);
+  const dataEmbedding: EmbeddingsData[] = [];
+  for (let i = 0; i < data.length; i++) {
+    dataEmbedding.push({
+      input: data[i],
+      embedding: embeddings.data[i].embedding,
+    });
+  }
+    saveDataToJsonFile(dataEmbedding, 'dataEmbedding.json')
+}
+
+createJsonFile()
